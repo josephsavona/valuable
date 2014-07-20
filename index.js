6b84f52748c5ec43826caa206489f19e18b49949
Joseph Savona <joe@josephsavona.com>
2014-07-19 17:19:00 -0700

    Float and Int types

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var Valueable = require('./src/valueable'),
     Map = require('./src/map'),
-    Struct = require('./src/struct'),
     List = require('./src/list'),
     Value = require('./src/value'),
+    Struct = require('./src/struct'),
+    Int = require('./src/types/int'),
+    Float = require('./src/types/float'),
     _ = require('lodash');
+
+// Valueable.register(_.isNumber, Float, 'Float');
+// Value.Float = Float;
 
 Valueable.register(_.isArray, List, 'List');
 Valueable.List = List;
@@ -14,7 +19,9 @@ Valueable.Map = Map;
 Valueable.register(function() { return true; }, Value, 'Value');
 Valueable.Value = Value;
 
-// struct has no auto-converter
+// stricter types have no auto-converter
 Value.Struct = Struct;
+Value.Int = Int;
+Valueable.Float = Float;
 
 module.exports = Valueable;
