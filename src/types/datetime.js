eb97e3aeb631695613c56a62ea6b2b6e98c779c1
Joseph Savona <joe@josephsavona.com>
2014-07-23 19:41:31 -0700

    start new DateTime and Func types

---
+++ src/types/datetime.js
@@ -0,0 +1,41 @@
+var assert = require('assert'),
+    _ = require('lodash'),
+    moment = require('moment'),
+    Value = require('../value'),
+    inherits = require('../inherits');
+
+/* 
+ * WORK IN PROGRESS
+ * the raw value should be a JavaScript Date - internally
+ * keep a momentjs object that is the source of truth
+ * and always create a new raw value from it whenever
+ * the user changes the date. 
+ */
+
+var DateTime = function DateTime() {
+  if (typeof this._raw === 'undefined') {
+    this._raw = moment();
+  }
+};
+
+var proto = {
+  assertValidValue: function DateTime$assertValidValue(val) {
+    var date = moment(val);
+    assert.ok(date.isValid(), 'DateTime(): value must be a JavaScript Date or momentjs date object');
+  },
+  negate: function DateTime$negate() {
+    this.setVal(!this._raw);
+  },
+  setVal: function DateTime$setVal(value) {
+    var rawValue = (value instanceof Value) ? value.val() : value,
+        date = moment(rawValue);
+    assert.ok(date.isValid(), 'DateTime(): value must be a JavaScript Date or momentjs date object');
+    this._raw = date;
+    this._notify();
+  },
+  add: function() {
+
+  }
+};
+
+module.exports = inherits(Value, DateTime, proto);
---
+++ src/types/func.js
@@ -0,0 +1,24 @@
+var assert = require('assert'),
+    _ = require('lodash'),
+    Value = require('../value'),
+    inherits = require('../inherits');
+
+var Func = function Func() {
+  assert.equal(typeof this._raw, 'function', 'Func(): must specify a function value');
+};
+
+var proto = {
+  assertValidValue: function Func$assertValidValue(val) {
+    assert.equal(typeof val, 'function', 'Func(): value must be a function');
+  },
+  call: function Func$call(bind, varargs) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    this._raw.apply(bind, args);
+  },
+  apply: function Func$apply(bind, args) {
+    assert.ok(_.isArray(args), 'Func(): apply takes single array of args');
+    this._raw.apply(bind, args);
+  }
+};
+
+module.exports = inherits(Value, Func, proto);
