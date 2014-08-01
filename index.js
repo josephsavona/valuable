8ed97a84dd101de879d5781e622e7dd65c97de73
Joseph Savona <joe@josephsavona.com>
2014-08-01 10:56:44 -0700

    initial store/lens

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var Valueable = require('./src/valueable'),
     Decimal = require('./src/types/decimal'),
     Bool = require('./src/types/bool'),
     Str = require('./src/types/str'),
+    store = require('./src/store'),
     _ = require('lodash');
 
 Valueable.register(_.isArray, List, 'List');
@@ -25,5 +26,8 @@ Valueable.Struct = Struct;
 Valueable.Decimal = Decimal;
 Valueable.Bool = Bool;
 Valueable.Str = Str;
+
+// data store
+Valueable.store = store;
 
 module.exports = Valueable;
---
+++ test/store_spec.js
@@ -0,0 +1,50 @@
+var assert = require('chai').assert,
+    sinon = require('sinon'),
+    _ = require('lodash'),
+    Valueable = require('..'),
+    rawValues = require('./mock_values');
+
+describe('Store() constructor', function() {
+  it('cannot create a store with an invalid definition', function() {
+    rawValues.forEach(function(val) {
+      assert.throws(function() {
+        Valueable.store(val);
+      });
+    });
+  });
+
+  it('can create a store', function() {
+    assert.doesNotThrow(function() {
+      var app = Valueable.store({
+        user: {
+          name: Valueable.Str,
+          age: Valueable.Decimal
+        }
+      });
+    }, 'can create a store');
+  });
+});
+
+describe.only('Store#execute()', function() {
+  var app;
+  beforeEach(function() {
+    app = Valueable.store({
+      users: {
+        name: Valueable.Str,
+        age: Valueable.Decimal
+      }
+    });
+  });
+
+  it('can execute a view over the data', function() {
+    var dude = {
+      name: 'dude',
+      age: 21
+    };
+    app.execute(function() {
+      var user = this.users.factory(dude);
+      this.commit(this.users.add(user));
+    });
+    assert.equal(app.users.length, 1);
+  });
+});
